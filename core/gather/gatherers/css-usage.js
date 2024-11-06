eb050d017c91965952fa11a4ae335daa19604070
هدية السعودية <83560449+hdyahksa@users.noreply.github.com>
2024-11-06 04:32:48 +0300

    Update css-usage.js 1
      المعدل

--- core/gather/gatherers/css-usage.js
+++ core/gather/gatherers/css-usage.js
@@ -1,43 +1,128 @@
-/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * @fileoverview Tracks unused CSS rules.
- */
-
-import BaseGatherer from '../base-gatherer.js';
-
-class CSSUsage extends BaseGatherer {
-  /** @type {LH.Gatherer.GathererMeta} */
-  meta = {
-    supportedModes: ['snapshot', 'navigation'],
-  };
-
-  /**
-   * @param {LH.Gatherer.Context} context
-   * @return {Promise<LH.Artifacts['CSSUsage']>}
-   */
-  async getArtifact(context) {
-    const session = context.driver.defaultSession;
-    const executionContext = context.driver.executionContext;
-
-    await session.sendCommand('DOM.enable');
-    await session.sendCommand('CSS.enable');
-    await session.sendCommand('CSS.startRuleUsageTracking');
-
-    // Force style to recompute.
-    // Doesn't appear to be necessary in newer versions of Chrome.
-    await executionContext.evaluateAsync('getComputedStyle(document.body)');
-
-    const {ruleUsage} = await session.sendCommand('CSS.stopRuleUsageTracking');
-    await session.sendCommand('CSS.disable');
-    await session.sendCommand('DOM.disable');
-
-    return ruleUsage;
-  }
+.navbar-brand img {
+       height: 168px;
+    width: 4height;
+    content: url(https://cdn.salla.sa/cdn-cgi/image/fit=scale-down,width=400,height=400,onerror=redirect,format=auto/oVql/j1zRTMxxyo6GX5M0ltChlNV5X1W238WXQmybUdsp.png);
+}
+.s-count-down-end-text, .btn-tab, .btn--collapse{
+background:black!important;
+color:white!important;}
+.my-button {
+  background-color: #4CAF50; /* لون الخلفية الأساسي */
+  color: white!important;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
 }
 
-export default CSSUsage;
+.my-button:hover {
+  background-color: #3e8e41!important; /* لون الخلفية عند التحويم */
+  color: black!important;
+}
+p:hover, a:hover {
+  color: white!important;
+}
+
+.bottom-header span, .store-footer .footer-container p{
+color:#fff!important;
+font-size:1rem;
+padding :5px;
+}
+.bottom-header h3{
+color:black;}
+.bottom-header i{
+background:#312519!important;
+padding:10px;
+}
+.footer-container, .s-block--testimonials .transition-shadow, .s-product-card-content{
+font-size:1.25rem!important;
+background:#312519!important;}
+.s-block__title {h2, a {
+font-size:1.5rem;
+color:#fff !important;
+}}
+.s-products-slider-card.swiper-slide {
+width:57%;
+padding-left:0.3rem;
+padding-left:0.3rem;
+}
+.store-footer {
+font-size:1.25rem;
+padding-top:5px;
+border-top:15px solid gold!important;
+  clip-path: polygon(0% 5%, 50% 0%, 100% 5%, 100% 100%, 0% 100%)!important;
+.footer-top, .to-top, .footer-container{
+ border-radius:10px;
+ padding:5px!important;
+border-bottom:1px solid #fff!important;
+opacity:1;
+}  
+.footer-list a{
+border-bottom:0.2px solid #fff;
+ padding:2px; 
+ border-radius:10px;
+ color:white!important;
+ }
+a.\!flex.flex-col.items-center.justify-center.text-sm.leading-none.gap-2{
+background:no-repeat!important;
+color:white;
+}}
+
+.s-block__display-all:after, .s-cart-summary-total{ display:none!important;
+}
+.carousel-slider .s-slider-block__title-nav {
+ display: flex;
+ 
+}
+salla-slider .s-slider-block__title-nav button .s-slider-button-icon svg {
+fill:#ffd700!important;
+ opacity:1; 
+ font-size:4rem!important;
+}
+ .s-block__display-all, .store-footer i {
+border-radius:9999px;
+color:gold;
+border: 2px solid gold;
+ padding:10px!important;
+ font-size:1.4rem; 
+fill:gold !important;
+ }
+body#app{.container{
+padding-left:0rem!important;
+ padding-right:0rem!important;
+  }}
+.s-slider-block__title{ display:flex; justify-content:center;}
+.s-button-wide:hover {
+  background: #ffd700!important;
+  color: #000!important;
+}
+.s-button-wide {
+border:2px solid #ffd700;
+background:#000!important;
+color:#ffd700!important;
+font-size:1.4rem;
+}
+.banner-square img, .home-slider img, .s-block--fixed-banner img{
+border:2px solid gold!important;
+border-radius:5px;
+padding:auto;}
+.s-slider-block__title h2, .store-footer h3 {
+border:2px solid gold;
+padding:5px;
+ border-radius:9999px;
+font-size:1.5rem;
+}
+
+.btn--share, .btn--wishlist, .sicon-quote, h4{
+ fill: gold !important;
+ background:no-repeat!important;
+color:gold!important; 
+ opacity:1;
+}
+.to-top p, .footer-bottom p{
+   font-size:1.25rem;
+border:2px solid gold;
+padding:8px;
+ border-radius:9999px;
+  }
